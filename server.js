e2247e7ff7ee36106c00481fcb910993222b39d4
Behnam Ghafary <behnam.ghafary@gmail.com>
2019-01-04 15:28:37 +0330

    change log

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ mongoose.connect(
 let server = http.createServer(createApp());
 // Start the app by listening on the default Heroku port
 server.listen(process.env.PORT || 8080, (req, res) => {
-  console.log(server.address());
   setInterval(() => {
+    console.log(server.address());
     request.put(
       `http://${server.address().address}:${server.address().port}/api/services/contact/${server.address().port}`,
       (err, response) => {
